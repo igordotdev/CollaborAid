18227e034ce02da4e95d98b26ca940c01919b79a
Wojtek <wojkor338@gmail.com>
2024-09-28 23:04:23 +0200

    backend fixed

--- src/back/manageNatural.ts
+++ src/back/manageNatural.ts
@@ -1,43 +1,8 @@
-import { serve } from "bun";
 import { Database } from "bun:sqlite";
+import { queryDatabase } from "./queryDatabase"
 
-function startNaturalTable(db: Database) {
-  // db.run("DROP TABLE IF EXISTS naturalEntities");
-
-  // Create the users table if it doesn't exist
-  if (
-    !db
-      .query(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='naturalEntities'"
-      )
-      .all()[0]
-  ) {
-    db.run(`
-      CREATE TABLE naturalEntities (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        firstName TEXT,
-        lastName TEXT,
-        age INTEGER,
-        email TEXT,
-        city TEXT,
-        password TEXT
-      );
-    `);
-    db.run(`INSERT INTO naturalEntities (firstName, lastName, age, email, city) VALUES
-      ('John', 'Doe', 30, 'johndoe@gmail.com', 'New York'),
-      ('Jane', 'Doe', 25, 'janedoe@latimes.com', 'Los Angeles');`);
-  }
-
-  // Define the common CORS headers
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Content-Type": "application/json",
-  };
-
-  // Helper function to build dynamic queries
-  function buildQuery(
+// Helper function to build dynamic queries
+function buildQueryNatural(
     baseQuery: string,
     filters: Record<string, any>
   ): { query: string; params: any[] } {
@@ -66,76 +31,37 @@ function startNaturalTable(db: Database) {
     return { query, params };
   }
 
-  // Define the server
-  serve({
-    port: 3000,
-    async fetch(req) {
-      const url = new URL(req.url, `http://${req.headers.get("host")}`);
+function startNaturalTable(db: Database) {
+  // db.run("DROP TABLE IF EXISTS naturalEntities");
 
-      if (req.method === "OPTIONS") {
-        // Handle preflight requests for CORS
-        return new Response(null, { headers: corsHeaders });
-      }
+  // Create the users table if it doesn't exist
+  if (
+    !db
+      .query(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='naturalEntities'"
+      )
+      .all()[0]
+  ) {
+    db.run(`
+      CREATE TABLE naturalEntities (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstName TEXT,
+        lastName TEXT,
+        age INTEGER,
+        email TEXT,
+        city TEXT,
+        password TEXT
+      );
+    `);
+    db.run(`INSERT INTO naturalEntities (firstName, lastName, age, email, city) VALUES
+      ('John', 'Doe', 30, 'johndoe@gmail.com', 'New York'),
+      ('Jane', 'Doe', 25, 'janedoe@latimes.com', 'Los Angeles');`);
+  }
 
-      try {
-        if (req.method === "GET" && url.pathname === "/api/naturalEntities") {
-          const filters = {
-            firstName: url.searchParams.get("fistName"),
-            lastName: url.searchParams.get("lastName"),
-            age: url.searchParams.get("age"),
-            email: url.searchParams.get("email"),
-            city: url.searchParams.get("city"),
-          };
+  queryDatabase(db, 3001);
 
-          const { query, params } = buildQuery(
-            "SELECT * FROM naturalEntities",
-            filters
-          );
-          const users = db.query(query).all(...params);
-          return new Response(JSON.stringify(users), { headers: corsHeaders });
-        } else if (
-          req.method === "POST" &&
-          url.pathname === "/api/naturalEntities"
-        ) {
-          // Ensure that the request body is in JSON format
-          const { firstName, lastName, age, email, city, password } = await req.json(); // Adjust based on your new form
-
-          // Insert user into the database
-          db.run(
-            "INSERT INTO naturalEntities (firstName, lastName, age, email, city, password) VALUES (?, ?, ?, ?, ?, ?)",
-            [firstName, lastName, age, email, city, password]
-          );
-          return new Response("User added successfully", {
-            status: 201,
-            headers: corsHeaders,
-          });
-        } else if (
-          req.method === "DELETE" &&
-          url.pathname.startsWith("/api/naturalEntities/")
-        ) {
-          const userId = Number(url.pathname.split("/")[3]);
-          db.run("DELETE FROM naturalEntities WHERE id = ?", [userId]);
-          return new Response("User deleted successfully", {
-            status: 200,
-            headers: corsHeaders,
-          });
-        } else {
-          return new Response("Not Found", {
-            status: 404,
-            headers: corsHeaders,
-          });
-        }
-      } catch (error) {
-        console.error("Error handling request:", error);
-        return new Response("Internal Server Error", {
-          status: 500,
-          headers: corsHeaders,
-        });
-      }
-    },
-  });
-
-  console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3001");
 };
 
 export { startNaturalTable };
+export { buildQueryNatural };
---
+++ src/back/queryDatabase.ts
@@ -0,0 +1,165 @@
+import { serve } from "bun";
+import { Database } from "bun:sqlite";
+import bcrypt from "bcrypt";
+import { buildQueryNatural } from "./manageNatural";
+import { buildQueryLegal } from "./manageLegal";
+
+// Define the common CORS headers
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Content-Type": "application/json",
+};
+
+function queryDatabase(db: Database, portNum: number) {
+  // Define the server
+  serve({
+    port: portNum,
+    async fetch(req) {
+      const url = new URL(req.url, `http://${req.headers.get("host")}`);
+
+      if (req.method === "OPTIONS") {
+        // Handle preflight requests for CORS
+        return new Response(null, { headers: corsHeaders });
+      }
+
+      try {
+        if (req.method === "GET" && url.pathname === "/api/naturalEntities") {
+          const filters = {
+            firstName: url.searchParams.get("fistName"),
+            lastName: url.searchParams.get("lastName"),
+            age: url.searchParams.get("age"),
+            email: url.searchParams.get("email"),
+            city: url.searchParams.get("city"),
+          };
+
+          const { query, params } = buildQueryNatural(
+            "SELECT * FROM naturalEntities",
+            filters
+          );
+          const users = db.query(query).all(...params);
+          return new Response(JSON.stringify(users), { headers: corsHeaders });
+        } else if (
+          req.method === "POST" &&
+          url.pathname === "/api/naturalEntities"
+        ) {
+          // Ensure that the request body is in JSON format
+          const { firstName, lastName, age, email, city, password } =
+            await req.json(); // Adjust based on your new form
+          const hashedPassword = bcrypt.hashSync(password, 10);
+          // Insert user into the database
+          db.run(
+            "INSERT INTO naturalEntities (firstName, lastName, age, email, city, password) VALUES (?, ?, ?, ?, ?, ?)",
+            [firstName, lastName, age, email, city, hashedPassword]
+          );
+          return new Response("User added successfully", {
+            status: 201,
+            headers: corsHeaders,
+          });
+        } else if (
+          req.method === "DELETE" &&
+          url.pathname.startsWith("/api/naturalEntities/")
+        ) {
+          const userId = Number(url.pathname.split("/")[3]);
+          db.run("DELETE FROM naturalEntities WHERE id = ?", [userId]);
+          return new Response("User deleted successfully", {
+            status: 200,
+            headers: corsHeaders,
+          });
+        } else if (
+          req.method === "GET" &&
+          url.pathname === "/api/legalEntities"
+        ) {
+          const filters = {
+            NIP: url.searchParams.get("NIP"),
+            REGON: url.searchParams.get("REGON"),
+            name: url.searchParams.get("name"),
+            legalForm: url.searchParams.get("legalForm"),
+            address: url.searchParams.get("address"),
+            dateOfStart: url.searchParams.get("dateOfStart"),
+            scopeOfActivities: url.searchParams.get("scopeOfActivities"),
+            mainValuesAndObjectives: url.searchParams.get(
+              "mainValuesAndObjectives"
+            ),
+            latestProjects: url.searchParams.get("latestProjects"),
+            contactNumber: url.searchParams.get("contactNumber"),
+            contactEmail: url.searchParams.get("contactEmail"),
+          };
+
+          const { query, params } = buildQueryLegal(
+            "SELECT * FROM legalEntities",
+            filters
+          );
+          const users = db.query(query).all(...params);
+          return new Response(JSON.stringify(users), { headers: corsHeaders });
+        } else if (
+          req.method === "POST" &&
+          url.pathname === "/api/legalEntities"
+        ) {
+          // Ensure that the request body is in JSON format
+          const {
+            NIP,
+            REGON,
+            name,
+            legalForm,
+            address,
+            dateOfStart,
+            ScopeOfActivities,
+            mainValuesAndObjectives,
+            latestProjects,
+            contactNumber,
+            contactEmail,
+            password,
+          } = await req.json(); // Adjust based on your new form
+          const hashedPassword = bcrypt.hashSync(password, 10);
+          // Insert user into the database
+          db.run(
+            "INSERT INTO legalEntities (NIP, REGON, name, legalForm, address, dateOfStart, ScopeOfActivities, mainValuesAndObjectives, latestProjects, contactNumber, contactEmail, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            [
+              NIP,
+              REGON,
+              name,
+              legalForm,
+              address,
+              dateOfStart,
+              ScopeOfActivities,
+              mainValuesAndObjectives,
+              latestProjects,
+              contactNumber,
+              contactEmail,
+              hashedPassword,
+            ]
+          );
+          return new Response("Company/NGO added successfully", {
+            status: 201,
+            headers: corsHeaders,
+          });
+        } else if (
+          req.method === "DELETE" &&
+          url.pathname.startsWith("/api/legalEntities/")
+        ) {
+          const REGONid = Number(url.pathname.split("/")[3]);
+          db.run("DELETE FROM legalEntities WHERE REGON = ?", [REGONid]);
+          return new Response("User deleted successfully", {
+            status: 200,
+            headers: corsHeaders,
+          });
+        } else {
+          return new Response("Not Found", {
+            status: 404,
+            headers: corsHeaders,
+          });
+        }
+      } catch (error) {
+        console.error("Error handling request:", error);
+        return new Response("Internal Server Error", {
+          status: 500,
+          headers: corsHeaders,
+        });
+      }
+    },
+  });
+}
+
+export { queryDatabase };
