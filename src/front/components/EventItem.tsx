5f496918d5349df2b4527b52190dbc2f3c1a403a
maciejwiacek <135031892+maciejwiacek@users.noreply.github.com>
2024-09-29 04:10:47 +0200

    Fixed events

--- src/front/components/EventItem.tsx
+++ src/front/components/EventItem.tsx
@@ -1,15 +1,15 @@
 export const EventItem = () => {
   return (
     <div
-      className={"flex bg-gray-100 rounded-xl w-10/12 mb-12 py-5 items-center"}
+      className={"flex bg-gray-100 rounded-xl w-10/12 mb-12 py-6 items-center"}
     >
       <div className={"text-center"}>
-        <p className={"text-red-800 text-6xl font-bold px-10"}>23</p>
-        <p className={"text-5xl font-bold"}>Wed</p>
+        <p className={"text-red-800 text-4xl font-bold px-10"}>23</p>
+        <p className={"text-2xl font-bold"}>Wed</p>
       </div>
       <div className={"w-0.5 h-20 bg-gray-300"}></div>
       <div className={"ml-10"}>
-        <p className={"text-4xl font-bold mb-2"}>Gadanie o niczym</p>
+        <p className={"text-3xl font-bold mb-2"}>Gadanie o niczym</p>
         <div className={"flex"}>
           <div
             className={
@@ -18,13 +18,13 @@ export const EventItem = () => {
           >
             <img
               src={"./public/assets/calendar-icon.svg"}
-              className={"w-5 mr-2"}
+              className={"w-4 mr-1"}
             />
-            <p>09:00 - 12:00</p>
+            <p className={"text-sm mt-0.5"}>09:00 - 12:00</p>
           </div>
           <div className={"flex"}>
-            <img src={"./public/assets/location-icon.svg"} className={"w-5"} />
-            <p>Online</p>
+            <img src={"./public/assets/location-icon.svg"} className={"w-4"} />
+            <p className={"text-sm mt-0.5 mr-1"}>Online</p>
           </div>
         </div>
       </div>
--- src/front/pages/events.tsx
+++ src/front/pages/events.tsx
@@ -4,7 +4,8 @@ const Events = () => {
   return (
     <>
       <div>
-        <h1 className={"text-6xl font-bold ml-28 mb-16 mt-14"}>Events</h1>
+        <h1 className={"text-6xl font-bold ml-28 mb-6 mt-14"}>Events</h1>
+        <h2 className={"text-3xl font-bold ml-28 mb-4"}>September</h2>
         <div
           className={"justify-center text-center items-center flex flex-col"}
         >
