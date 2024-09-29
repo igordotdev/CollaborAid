99d5e66099425aa141959511b8fdbd1c2d806e96
maciejwiacek <135031892+maciejwiacek@users.noreply.github.com>
2024-09-29 04:06:06 +0200

    Events

--- src/front/pages/events.tsx
+++ src/front/pages/events.tsx
@@ -1,11 +1,20 @@
+import { EventItem } from "../components/EventItem.tsx";
+
 const Events = () => {
-		return (
-			<>
-				<div>
-						<h1>Events</h1>
-				</div>
-			</>
-		)
+  return (
+    <>
+      <div>
+        <h1 className={"text-6xl font-bold ml-28 mb-16 mt-14"}>Events</h1>
+        <div
+          className={"justify-center text-center items-center flex flex-col"}
+        >
+          <EventItem />
+          <EventItem />
+          <EventItem />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Events;
